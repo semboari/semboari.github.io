9ac5eabb6a715fdb262de071c8a94033c1eccc10
reinhard semboari <reinhardsemboari471@gmail.com>
2020-01-09 22:59:23 +0900

    view-pemeriksa-dan-rektor-fix

--- apps/js/routes/pemeriksa.router.js
+++ apps/js/routes/pemeriksa.router.js
@@ -1,7 +1,27 @@
-angular.module('app.pemeriksa.router', [ 'ui.router' ]).config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/account/login');
+angular.module('app.pemeriksa.router', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/pemeriksa/home');
 	$stateProvider.state('pemeriksa', {
 		url: '/pemeriksa',
 		templateUrl: '../apps/views/pemeriksa/menu-pemeriksa.html'
-	});
+	})
+
+	$stateProvider.state('pemeriksa-home', {
+		url: '/home',
+		parent: 'pemeriksa',
+		templateUrl: '../apps/views/pemeriksa/home.html'
+	})
+
+	$stateProvider.state('pemeriksa-pengusul-penelitian', {
+		url: '/pengusul-penelitian',
+		parent: 'pemeriksa',
+		templateUrl: '../apps/views/pemeriksa/pengusul-penelitian.html'
+	})
+
+	$stateProvider.state('pemeriksa-berkas-pengusulan-penelitian', {
+		url: '/berkas-pengusulan-penelitian',
+		parent: 'pemeriksa',
+		templateUrl: '../apps/views/pemeriksa/berkas-pengusulan-penelitian.html'
+	})
+
+	;
 });
--- apps/js/routes/rektor.router.js
+++ apps/js/routes/rektor.router.js
@@ -1,7 +1,27 @@
-angular.module('app.rektor.router', [ 'ui.router' ]).config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/account/login');
+angular.module('app.rektor.router', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/rektor/home');
 	$stateProvider.state('rektor', {
 		url: '/rektor',
 		templateUrl: '../apps/views/rektor/menu-rektor.html'
-	});
+	})
+
+	$stateProvider.state('rektor-home', {
+		url: '/home',
+		parent: 'rektor',
+		templateUrl: '../apps/views/rektor/home.html'
+	})
+
+	$stateProvider.state('rektor-pengusul', {
+		url: '/pengusul',
+		parent: 'rektor',
+		templateUrl: '../apps/views/rektor/pengusul.html'
+	})
+
+	$stateProvider.state('rektor-berkas-pengusulan', {
+		url: '/berkas-pengusulan',
+		parent: 'rektor',
+		templateUrl: '../apps/views/rektor/berkas-pengusulan.html'
+	})
+
+	;
 });
