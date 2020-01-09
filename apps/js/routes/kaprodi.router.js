6b9901744048bf81cc6018d97bad99ebec02918d
reinhard semboari <reinhardsemboari471@gmail.com>
2020-01-09 21:41:13 +0900

    view-kaprodi

--- apps/js/routes/kaprodi.router.js
+++ apps/js/routes/kaprodi.router.js
@@ -1,7 +1,15 @@
-angular.module('app.kaprodi.router', [ 'ui.router' ]).config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/account/login');
+angular.module('app.kaprodi.router', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/kaprodi/home');
 	$stateProvider.state('kaprodi', {
 		url: '/kaprodi',
 		templateUrl: '../apps/views/kaprodi/menu-kaprodi.html'
-	});
+	})
+
+	$stateProvider.state('kaprodi-home', {
+		url: '/home',
+		parent: 'kaprodi',
+		templateUrl: '../apps/views/kaprodi/home.html'
+	})
+
+	;
 });
