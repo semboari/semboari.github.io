ca1c849b620f261afec07aa4fc0f11c5acf293fe
ocph23@gmail.com <ocph23@gmail.com>
2020-01-16 03:56:29 +0900

    aa

--- apps/js/routes/admin.router.js
+++ apps/js/routes/admin.router.js
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
-angular.module('app.admin.router', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
-
-	$stateProvider.state('main	', {
-		url: '/',
-		parent: 'account',
-		controller: 'LoginController',
-		templateUrl: '../apps/views/accounts/sign-in.html'
-	})
-=======
 angular.module('app.admin.router', [ 'ui.router' ]).config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/admin/home');
->>>>>>> 7ec534fd96b100e4f93140137f5c5653cf9a2890
 	$stateProvider.state('admin', {
 		url: '/admin',
 		templateUrl: '../apps/views/admin/menu-admin.html'
 	});
 
 	$stateProvider.state('admin-home', {
-
 		url: '/home',
 		parent: 'admin',
 		templateUrl: '../apps/views/admin/home.html'
