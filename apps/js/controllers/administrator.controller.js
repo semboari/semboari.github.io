ebd9286e2d077d57f17f2bff336110aafa0092f6
reinhard semboari <reinhardsemboari471@gmail.com>
2020-01-26 00:19:57 +0900

    change

--- apps/js/controllers/administrator.controller.js
+++ apps/js/controllers/administrator.controller.js
@@ -3,3 +3,15 @@ angular
 	.controller('administrator.controller', administratorController)
 	.controller('administrator.home.controller', administratorHomeController)
 	.controller('administrator.dosen.controller', administratorDosenController);
+
+function administratorDosenController($scope, AuthService, PenilaianService) {
+	AuthService.Init(['dosen']);
+}
+
+function administratorHomeController($scope, AuthService, PenilaianService) {
+	AuthService.Init(['dosen']);
+}
+
+function administratorController($scope, AuthService, PenilaianService) {
+	AuthService.Init(['dosen']);
+}
--- apps/js/routes/administrator.router.js
+++ apps/js/routes/administrator.router.js
@@ -1,4 +1,4 @@
-angular.module('administrator.router', [ 'ui.router' ]).config(function($stateProvider, $urlRouterProvider) {
+angular.module('administrator.router', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
 	$stateProvider
 		.state('administrator', {
 			url: '/administrator',
@@ -12,10 +12,26 @@ angular.module('administrator.router', [ 'ui.router' ]).config(function($statePr
 			templateUrl: '../apps/views/administrator/home.html'
 		})
 		.state('administrator-dosen', {
-			url: '/fakultas',
+			url: '/dosen',
 			parent: 'administrator',
 			controller: 'administrator.dosen.controller',
 			templateUrl: '../apps/views/administrator/dosen.html'
-		});
+		})
+
+		.state('administrator-fakultas', {
+			url: '/fakultas',
+			parent: 'administrator',
+			controller: 'administrator.fakultas.controller',
+			templateUrl: '../apps/views/administrator/fakultas.html'
+		})
+
+		.state('administrator-progdi', {
+			url: '/progdi',
+			parent: 'administrator',
+			controller: 'administrator.progdi.controller',
+			templateUrl: '../apps/views/administrator/progdi.html'
+		})
+
+	;
+
 });
- s
